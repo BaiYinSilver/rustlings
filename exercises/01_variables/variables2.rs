3777e9419effeb6546eb6339fd428e82a4ce5b0a
Yin Bai <silvervista@gmail.com>
2023-03-28 11:05:46 -0300

    update as of march 28

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,7 +1,9 @@
-fn main() {
-    // TODO: Change the line below to fix the compiler error.
-    let x;
+// variables2.rs
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
+fn main() {
+    let x = 32;
+    println!("x is {}", x);
     if x == 10 {
         println!("x is ten!");
     } else {
