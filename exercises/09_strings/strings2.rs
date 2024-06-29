f11fd3903105323e1625c617393ddcb380d5c057
BaiYinSilver <silvervista@gmail.com>
2024-06-29 00:51:22 -0700

    feat: jun 29 2024 update

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -1,14 +1,24 @@
-// TODO: Fix the compiler error in the `main` function without changing this function.
-fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
-}
+// strings2.rs
+// Make me compile without changing the function signature!
+// Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
+
 
 fn main() {
-    let word = String::from("green"); // Don't change this line.
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    if is_a_color_word(word) {
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("string is {}", s);
+
+    let word = String::from("green"); // Try not changing this line :)
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
+}
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
 }
